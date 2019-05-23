8c74ce9eb0debaa862e1b5d505243bfc8cc67969
dekarupovi <denis.karupovic@etu.uca.fr>
2019-05-23 09:52:03 +0200

    list with button to see randonnee detail

--- src/app/entities/hike.ts
+++ src/app/entities/hike.ts
@@ -1,10 +1,45 @@
 export class Hike {
-    private name;
-    private date;
-    private nbPeople;
-    constructor(name, date, nbPeople) {
-        this.name = name;
-        this.date = date;
-        this.nbPeople = nbPeople;
+    private _id;
+    private _name;
+    private _date;
+    private _nbPeople;
+    constructor(id, name, date, nbPeople) {
+        this._id = id;
+        this._name = name;
+        this._date = date;
+        this._nbPeople = nbPeople;
+    }
+
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        this._id = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    set date(value) {
+        this._date = value;
+    }
+
+    get nbPeople() {
+        return this._nbPeople;
+    }
+
+    set nbPeople(value) {
+        this._nbPeople = value;
     }
 }
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -9,24 +9,24 @@ import {Hike} from '../entities/hike';
 export class ListPage implements OnInit {
   private selectedItem: any;
   private hikings = [
-    new Hike('Paris-Dakar', '2019-05-17', 26),
-    new Hike('Clermont-LePuy', '2019-06-23', 31),
-    new Hike('Bankok', '2019-09-15', 22),
-    new Hike('Lyon', '2019-05-27', 58),
-    new Hike('New-York', '2019-08-17', 278),
-    new Hike('Bois de Boulogne', '2019-02-17', 963),
+    new Hike(1, 'Paris-Dakar', '2019-05-17', 26),
+    new Hike(2, 'Clermont-LePuy', '2019-06-23', 31),
+    new Hike(3, 'Bankok', '2019-09-15', 22),
+    new Hike(4, 'Lyon', '2019-05-27', 58),
+    new Hike(5, 'New-York', '2019-08-17', 278),
+    new Hike(6, 'Bois de Boulogne', '2019-02-17', 963),
   ];
-    public items: Array<{ name: string; date: Date; nbPeople: number }> = [];
+    public items: Array<{ id: number, name: string; date: Date; nbPeople: number }> = [];
     constructor() {
-        for (let i = 1; i < this.hikings.length; i++) {
+        for (let i = 1; i <= this.hikings.length; i++) {
             this.items.push({
-                name: i.name,
-                date: i.date,
-                nbPeople: i.nbPeople
+                id : this.hikings[i - 1].id,
+                name: this.hikings[i - 1].name,
+                date: this.hikings[i - 1].date,
+                nbPeople: this.hikings[i - 1].nbPeople
             });
         }
     }
-
   ngOnInit() {
   }
   // add back when alpha.4 is out
