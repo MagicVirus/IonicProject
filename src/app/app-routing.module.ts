34c49a23fb763e24d9a29ed86abd2a08ad2b152d
Denis KARUPOVIC <d.karupovic@hotmail.com>
2019-06-03 15:22:13 +0200

    cassage des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
-  { path: 'page2', loadChildren: './page2/page2.module#Page2PageModule' },
-  { path: 'page3', loadChildren: './page3/page3.module#Page3PageModule' },
-  { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './public/register/register.module#RegisterPageModule' },
-  { path: 'dashboard', loadChildren: './members/dashboard/dashboard.module#DashboardPageModule' },
-  { path: 'hike-detail', loadChildren: './hike-detail/hike-detail.module#HikeDetailPageModule' },
+    { path: '', redirectTo: 'list', pathMatch: 'full' },
+    { path: 'list', loadChildren: './list/list.module#ListPageModule' },
+    { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
+    { path: 'register', loadChildren: './public/register/register.module#RegisterPageModule' },
+    { path: 'dashboard', loadChildren: './members/dashboard/dashboard.module#DashboardPageModule' },
+    { path: 'hike-detail', loadChildren: './hike-detail/hike-detail.module#HikeDetailPageModule' },
+    { path: 'members', canActivate: [AuthGuard], loadChildren: './members/member-routing.module#MemberRoutingModule' },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- src/app/guard.guard.spec.ts
+++ src/app/guard.guard.spec.ts
@@ -3,13 +3,13 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { GuardGuard } from './guard.guard';
 
 describe('GuardGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [GuardGuard]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [GuardGuard]
+        });
     });
-  });
 
-  it('should ...', inject([GuardGuard], (guard: GuardGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+    it('should ...', inject([GuardGuard], (guard: GuardGuard) => {
+        expect(guard).toBeTruthy();
+    }));
 });
